eacd6e5c91eb9c643b4d636d1765cfbb235713b3
solarslurpi <happyday.mjohnson@gmail.com>
2021-12-11 12:50:58 -0800

    debugging fixes.

--- carbot.py
+++ carbot.py
@@ -1,7 +1,12 @@
 import logging
 import sys
+from enum import Enum
 import paho.mqtt.client as mqtt
 from adafruit_servokit import ServoKit
+class Movement(Enum):
+    FORWARD=1
+    BACKWARD=2
+    STOP=3
 # Declare the servo motors...
 kit = ServoKit(channels=16)
 # 
@@ -15,18 +20,46 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 # 
 # Handle the throttle speed across requests for speed changes.
-def car_speed(direction='up'):
-    logger.debug('--> in car_speed direction = ' + direction)
-    if not hasattr(car_speed,'throttle'):
-        car_speed.throttle = .5
-    if direction == 'up':
-        car_speed.throttle = round(car_speed.throttle + .1 if car_speed.throttle < 1 else car_speed.throttle,1)
-    elif direction == 'down':
-        car_speed.throttle = round(car_speed.throttle - .1 if car_speed.throttle > .1 else car_speed.throttle,1)
-    else:
-        pass
-    logger.debug('The throttle speed has been set to '+car_speed.throttle)
-    return car_speed.throttle
+class carbot:
+    # The Servos don't turn the way I think they should.
+    # The one connected to 0 goes the opposite direction than the other...
+    def __init__(self):
+        self.throttle_speed = .1
+        self.command = None
+        self.prev_movement = Movement.STOP
+    def forward(self):
+
+        kit.continuous_servo[0].throttle = -self.throttle_speed
+        kit.continuous_servo[1].throttle = self.throttle_speed
+        logger.debug("^^^ moving forward! ^^^ at speed "+str(self.throttle_speed))
+        self.prev_movement = Movement.FORWARD
+    def backward(self):
+ 
+        kit.continuous_servo[0].throttle = self.throttle_speed
+        kit.continuous_servo[1].throttle = -self.throttle_speed
+        self.prev_movement = Movement.BACKWARD
+        logger.debug("^^^ moving backward! ^^^ at speed "+str(self.throttle_speed))
+    def stop(self):
+        kit.continuous_servo[0].throttle = 0
+        kit.continuous_servo[1].throttle = 0
+        self.prev_movement = Movement.STOP
+        logger.debug("^^^ stopped! ^^^")
+    def slow_down(self):
+        self.throttle_speed = round(self.throttle_speed - .1 if self.throttle_speed > .1 else self.throttle_speed,1)
+        if self.prev_movement == Movement.FORWARD:
+            self.forward()
+        elif self.prev_movement == Movement.BACKWARD:
+            self.backward()
+        logger.debug("^^^ slowed speed down to "+ str(self.throttle_speed))
+    def speed_up(self):
+        self.throttle_speed = round(self.throttle_speed + .1 if self.throttle_speed < 1 else self.throttle_speed,1)
+        if self.prev_movement == Movement.FORWARD:
+            self.forward()
+        elif self.prev_movement == Movement.BACKWARD:
+            self.backward()
+        logger.debug("^^^ speeded up to "+str(self.throttle_speed))
+
+bot = carbot()
 # 
 #  Set up mosquitto sub.
 client_name='carbot'
@@ -50,32 +83,21 @@ def on_message(client, userdata, msg):
 
     # MOVE THE CAR FORWARD
     if message == "f":
-        logger.debug("^^^ moving forward! ^^^")
-        kit.continuous_servo[0].throttle = car_speed('none')
-        kit.continuous_servo[1].throttle = car_speed('none')
+        bot.forward()
     # STOP THE CAR
     elif message == "s":
-        logger.debug("!!! stopping!")
-        kit.continuous_servo[0].throttle = 0
-        kit.continuous_servo[1].throttle = 0
+        bot.stop()
     # MOVE THE CAR BACKWARD
     elif message == "b":
-        logger.debug("\/ backward \/")
-        kit.continuous_servo[0].throttle = -car_speed('none')
-        kit.continuous_servo[1].throttle = -car_speed('none')
+        bot.backward()
     # SPEED UP THE CAR
     elif message == '+':
-        logger.debug("!!! speeding up!")
-        kit.continuous_servo[0].throttle = car_speed('up')
-        kit.continuous_servo[1].throttle = car_speed('up')
-        
-    # SLOW THE CAR DOWN
+        bot.speed_up()
+    # SLOW DOWN THE CAR
     elif message == '-':
-        logger.debug("!!! slowing down!")
-        kit.continuous_servo[0].throttle = car_speed('down')
-        kit.continuous_servo[1].throttle = car_speed('down')
-    else:
-        logger.debug('-- Did not understand the command -- :' + message)
+        bot.slow_down()
+        
+  
 
 # # 
 # 
